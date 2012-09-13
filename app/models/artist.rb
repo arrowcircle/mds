19d00b6e9fc0996d5fe0abc4b0da90aaa7da6a0c
Oleg Bovykin mbpr <oleg.bovykin@gmail.com>
2012-09-14 00:49:37 +0400

    added artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,14 @@
 class Artist < ActiveRecord::Base
   attr_accessible :name
   has_many :tracks, :dependent => :destroy
+  validates :name, :presence => true, :uniqueness => true
 
-  def self.search q
-    res = []
-    res = Artist.where("name ILIKE ?", "#{q}%").order(:name) if q && q.length >= 1
-    res
+  def self.search(query)
+    if query && query.length > 0
+      Artist.includes(:tracks).where("artists.name ILIKE ?", "#{query}%").order("artists.name")
+    else
+      Artist.includes(:tracks).order("artists.name")
+    end
   end
 
   def to_param
