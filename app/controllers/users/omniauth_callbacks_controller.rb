49f2b10d969a65ade5c862407a01db5030db59bd
Oleg Bovykin mbpr <oleg.bovykin@gmail.com>
2012-09-14 18:03:06 +0400

    fix logout link

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,11 +13,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def twitter
     @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
+    logger.info request.env["omniauth.auth"]
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Twitter"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      session["devise.twitter_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
@@ -30,7 +31,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      session["devise.vkontakte_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
