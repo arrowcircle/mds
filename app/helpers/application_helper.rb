fb999af4dc6c50f058e635351ab6a95478d16e88
Oleg Bovykin mbpr <oleg.bovykin@gmail.com>
2012-10-09 01:52:03 +0400

    added default avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,20 @@ module ApplicationHelper
   def link_to_remove_fields(name, f)
     f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", :class => :del)
   end
-  
+
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
     link_to_function(name, ("add_fields(this, '#{association}', '#{escape_javascript(fields)}')"))
+  end
+
+  def user_avatar_link user
+    if user.avatar.url
+      link_to image_tag(user.avatar.url, :size => "32x32", :alt => user.username), user
+    else
+      link_to image_tag("no_avatar.png", :size => "32x32", :alt => user.username), user
+    end
   end
 end
