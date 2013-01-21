1bee0bdf5b9560dfdf4ec3c4571c8b84fce00c09
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-01-22 03:33:30 +0400

    fixed db sync

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,14 +57,15 @@ namespace :sync do
   task :db do
     get("#{current_path}/config/database.yml", "tmp/database.yml")
 
-    remote_settings = YAML::load_file("config/database.yml")["production"]
+    remote_settings = YAML::load_file("tmp/database.yml")["production"]
     local_settings  = YAML::load_file("config/database.yml")["development"]
 
     run "export PGPASSWORD=#{remote_settings["password"]} && pg_dump --host=#{remote_settings["host"]} --port=#{remote_settings["port"]} --username #{remote_settings["username"]} --file #{current_path}/tmp/#{remote_settings["database"]}_dump --no-owner -Fc #{remote_settings["database"]}"
 
-    run_locally "rsync --recursive --times --rsh=ssh --compress --human-readable --progress #{current_path}/tmp/#{remote_settings["database"]}_dump tmp/"
+    run_locally "rsync --recursive --times --rsh=ssh --compress --human-readable --progress webmaster@redde.ru:#{current_path}/tmp/#{remote_settings["database"]}_dump tmp/"
 
     run_locally "dropdb -U #{local_settings["username"]} --host=#{local_settings["host"]} --port=#{local_settings["port"]} #{local_settings["database"]}"
+
     run_locally "createdb -U #{local_settings["username"]} --host=#{local_settings["host"]} --port=#{local_settings["port"]} -T template0 #{local_settings["database"]}"
     run_locally "pg_restore -U #{local_settings["username"]} --host=#{local_settings["host"]} --port=#{local_settings["port"]} -d #{local_settings["database"]} --no-owner tmp/#{remote_settings["database"]}_dump"
   end
