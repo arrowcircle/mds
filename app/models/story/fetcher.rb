3a2361fc61c4ef45dfd443003dfcada89805bde6
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-02-23 16:21:53 +0400

    refactoring

--- app/models/story/fetcher.rb
+++ app/models/story/fetcher.rb
@@ -21,8 +21,25 @@ module Story::Fetcher
     @results ||= get_results
   end
 
-  def parse_result_tr tr
-    {}.merge!(:position => tr.search("td")[0].search("a")[1].inner_text, :link_to_page => tr.search("td")[0].search("a")[1].attributes["href"].value, :date => tr.search("td")[3].inner_text, :station => tr.search("td")[5].inner_text)
+  def extract_position tds
+    tds[0].search("a")[1].inner_text
+  end
+
+  def extract_link tds
+    tds[0].search("a")[1].attributes["href"].value
+  end
+
+  def extract_date tds
+    tds[3].inner_text
+  end
+
+  def extract_tration tds
+    tds[5].inner_text
+  end
+
+  def parse_result_tr tds
+    tds = tr.search("td")
+    {}.merge!(:position => extract_position(tds), :link_to_page => extract_link(tds), :date => extract_date(tds), :station => extract_station(tds))
   end
 
   def get_results
