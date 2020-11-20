b1f1eded5a4dee29703450123e518afb1b112e1a
Oleg Bovykin <oleg.bovykin@gmail.com>
2020-11-20 19:55:41 +0300

    Добавил опенграф

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,39 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
-  def configure_permitted_parameters
-    keys = [:username, :email, :avatar]
-    [:sign_up, :sign_in, :account_update].each do |key|
-      devise_parameter_sanitizer.permit(key, keys: keys)
+    def configure_permitted_parameters
+      keys = [:username, :email, :avatar]
+      [:sign_up, :sign_in, :account_update].each do |key|
+        devise_parameter_sanitizer.permit(key, keys: keys)
+      end
     end
-  end
+
+  private
+    def metadata
+      @metadata ||= OpenStruct.new(title: title, description: description, og: og)
+    end
+    helper_method :metadata
+
+    def og
+      {}
+    end
+
+    def title
+      'Проект с плейлистами из радиопередачи модель для сборки'
+    end
+    helper_method :title
+
+    def description
+      'Мы собираем плейлисты из рассказов, которые игрались в эфире передачи модель для сборки'
+    end
+    helper_method :description
+
+    def tags
+      'Модель для сборки, что играет, опознать трек, мдс'
+    end
+    helper_method :tags
 end
---
+++ app/helpers/opengraph_helper.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module OpengraphHelper
+  def facebook_opengraph(options = {})
+    {
+      'og:title' => title,
+      'og:type' => 'website',
+      'og:url' => request.original_url,
+      'og:image' => card_image,
+      'og:description' => description,
+      'og:site_name' => 'МДС Музыка',
+      'og:locale' => 'ru_RU'
+    }.merge(options)
+  end
+
+  def twitter_cards(options = {})
+    {
+      'twitter:card' => 'summary_large_image',
+      'twitter:title' => title,
+      'twitter:description' => description,
+      'twitter:image:src' => card_image
+    }.merge(options)
+  end
+
+  def card_image
+    ''
+  end
+end
