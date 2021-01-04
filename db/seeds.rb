ff9eba0d7da9a2bcf3a47d7af78aeb43d6f8ae70
Oleg Bovykin <oleg.bovykin@gmail.com>
2021-01-04 16:27:28 +0300

    Первый заход на деплой в k8s

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def s3_test_client
+  @s3_test_client ||= Aws::S3::Client.new(
+    region: 'us-east-1',
+    access_key_id: ENV['S3_ACCESS_KEY'],
+    secret_access_key: ENV['S3_SECRET'],
+    endpoint: ENV['S3_HOST'],
+    force_path_style: true
+  )
+end
+
+def create_bucket(name)
+  s3_test_client.create_bucket(bucket: name)
+rescue
+  true
+end
+
+%w(images cache).each { |n| create_bucket(n) }
