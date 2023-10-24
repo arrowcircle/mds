5d8bc8a552d83c1ccb478e04fb905cfe235a655a
Oleg Bovykin <oleg.bovykin@gmail.com>
2023-10-24 17:35:59 +0200

    SMall fixes to deploy and templates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000'
+  }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -104,10 +107,10 @@ Rails.application.configure do
     domain: "mds.redde.ru",
     address: ENV["SMTP_HOST"],
     port: ENV["SMTP_PORT"]
-    # user_name: ENV['SMTP_USER'],
-    # password: ENV['SMTP_PASSWORD'],
-    # authentication: :login,
-    # enable_starttls_auto: true
+    user_name: ENV['SMTP_USER'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true
   }
 
   # LOGS
