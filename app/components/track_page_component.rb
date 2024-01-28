726b2ca44dfb1873bf5de56593c4a451b5b29630
Oleg Bovykin <oleg.bovykin@gmail.com>
2024-01-28 16:08:11 +0100

    Fixed track page

--- app/components/track_page_component.rb
+++ app/components/track_page_component.rb
@@ -1,5 +1,6 @@
 class TrackPageComponent < ViewComponent::Base
   attr_reader :artist, :track, :stories, :current_user
+
   def initialize(artist:, track:, stories:, current_user: nil)
     @artist = artist
     @track = track
@@ -8,17 +9,16 @@ class TrackPageComponent < ViewComponent::Base
   end
 
   def buttons
-    [
-      PageTitleButtonComponent.new(title: "Добавить", icon: "images/add.svg", link: [:new, :artist], mobile_visible: true)
-    ]
+    res = []
+    res << PageTitleButtonComponent.new(title: "Редактировать", icon: "images/edit.svg", link: [:edit, artist, track], mobile_visible: true) if current_user&.admin?
+    res << PageTitleButtonComponent.new(title: "Назад", icon: "images/arrow_left.svg", link: [artist], mobile_visible: true)
   end
 
   def title
-    return "Играет в рассказах" if @artists.present?
-    "Мы не нашли такого артиста"
-  end
-
-  def metadata
-    []
+    res = []
+    res << link_to(artist.name, [:artist], class: :link)
+    res << "-"
+    res << track.name
+    raw res.join(" ")
   end
 end
